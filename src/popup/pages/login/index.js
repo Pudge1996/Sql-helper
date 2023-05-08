9e2c0b4f733970f50cb88245d46e217a9ff24808
Cenkaifeng <jervisc@ones.com>
2023-05-08 14:54:43 +0800

    docs: 更新文档 + 模型类型复选

--- src/popup/pages/login/index.js
+++ src/popup/pages/login/index.js
@@ -1,12 +1,19 @@
 import { useNavigate  } from 'react-router-dom'
-import { Button, Input } from 'antd'
+import { Button, Input, Select } from 'antd'
 import imgLogo from './logo.png'
 import './login.styl'
 
+const { Option } = Select
 // demo 练手页，后续以此基础改为表单页
 export default function Login() {
     // 路由钩子
     const navigate = useNavigate();
+    const [option, setOption] = useState('react')
+
+    // 随着Select的选择变化，及时更新option
+    const handleSelectChange = (value) => {
+        setOption(value)
+    }
 
     // 初始完成后跳到操作界面
     const toChat = () => {
@@ -22,6 +29,18 @@ export default function Login() {
             <div className="ipt-con">
                 <label>session-key:</label>
                 <Input.Password placeholder="session-key"/>
+            </div>
+            <div className="ipt-con">
+                <label>Modal-type:</label>
+                <Select
+                    value={option}
+                    onChange={handleSelectChange}
+                    style={{ width: '100%' }}
+                >
+                    <Option value="gpt-3.5-turbo">gpt-3.5-turbo</Option>
+                    <Option value="gpt-4-32k">gpt-3.0</Option>
+                    <Option value="gpt-4-32k">gpt-4-32k</Option>
+                </Select>
             </div>
             <div className="ipt-con">
                 <Button type="primary" block={true} onClick={toChat}>初始化</Button>
